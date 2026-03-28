6d57f097860907ebd51756f3743692b1436cd438
landonmobley04302012-sys <landonmobley04302012@gmail.com>
2026-03-28 06:54:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/test', (req, res) => res.json({ status: 'ok' }));
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
